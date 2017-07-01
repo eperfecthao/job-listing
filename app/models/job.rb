76d868d6d8505d8939846de0089fe4eabfc99043
songhao <songhao@songhaodeMacBook-Pro.local>
2017-07-02 07:50:45 +0800

    add lower/upper bound and contact email

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,5 +1,8 @@
 class Job < ApplicationRecord
   validates_presence_of :title
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
+  validates_presence_of :wage_upper_bound
 
   scope :recent, -> {order("created_at DESC")}
 end
